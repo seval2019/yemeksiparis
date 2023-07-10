86c271d71d0fc75153ef7861955b829c5d613db0
Seval Senturk <seval.senturk@tgs.aero>
2023-07-10 11:24:51 +0300

    campaigns component ended

--- components/Campaigns.jsx
+++ components/Campaigns.jsx
@@ -5,7 +5,7 @@ import { MdShoppingCart } from "react-icons/md";
 const CampaignItem = () => {
   return (
     <div className="bg-secondary flex-1 rounded-md py-5 px-[15px] flex items-center gap-x-4">
-      <div className="relative md:w-44 md:h-44 w-36 h-36 after:content-['']  border-[5px] border-primary rounded-full overflow-hidden">
+      <div className="relative sm:w-44 md:h-44 w-36 h-36 after:content-['']  border-[5px] border-primary rounded-full overflow-hidden">
         <Image
           src="/images/o1.jpg"
           alt=""
@@ -20,8 +20,8 @@ const CampaignItem = () => {
           <span className="text-[40px]">20%</span>
           <span className="text-sm inline-block ml-1">Off</span>
         </div>
-        <button className="btn-primary flex items-center gap-x-2">
-          Order Now <MdShoppingCart size={20} />
+        <button className="btn-primary flex items-center gap-x-2 rounded-full">
+           Order Now <MdShoppingCart size={20} />
         </button>
       </div>
     </div>
