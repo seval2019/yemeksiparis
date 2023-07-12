349684cf67b87c794ea965803560a47dd068bab9
Seval Senturk <seval.senturk@tgs.aero>
2023-07-12 16:25:14 +0300

    home page design 3

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,22 @@
 import Image from 'next/image'
-
-import Home from "./home"
 import Head from "next/head";
 import Header from '../components/layout/Header';
-
+import Title from '../components/ui/Title';
+import Home from "./home";
 
 export default function Index() {
   return (
     <div className="">
       <Head>
-        <title>Create Next App</title>
+        <Title>Create Next App</Title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
       </Head>
-      <Header/>
-      <Home/>
+      <Header />
+      <Home />
+      <div className="p-20 container mx-auto">
+      </div>
     </div>
   )
 }
